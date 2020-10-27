1ea967004fcae97ba20be49249cced065d45d802
JunHyeock Park <wnsgur6311@gmail.com>
2020-10-28 07:50:27 +0900

    Exercise Koa Basic

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 const Koa = require('koa');
+const Router = require('koa-router');
+const bodyparser = require('koa-bodyparser');
+
+const api = require('./api');
 
 const app = new Koa();
+const router = new Router();
+
+router.use('/api', api.routes());
+
+app.use(bodyparser());
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(4000, () => {
   console.log('Listening to port 4000');
